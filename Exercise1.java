473ef974f19d21d33778e35500f805968f8cf667
Adam Bemiller <abemille@villanova.edu>
2014-04-09 20:31:06 -0400

    Initial commit

---
+++ Exercise1.java
@@ -0,0 +1,27 @@
+import java.nio.file.*;
+
+public class Exercise1 {
+
+    /**
+     * @param args
+     * 
+     *            Write an example that counts the number of times a particular
+     *            character, such as e, appears in a file. The character can be
+     *            specified at the command line. You can use xanadu.txt as the
+     *            input file.
+     */
+    public static void main(String[] args) {
+
+        Path textFile = Paths.get("xanadu.txt");
+
+        Exercise1HelperMethods methods = new Exercise1HelperMethods();
+
+        if (methods.isAccessible(textFile) && Files.isReadable(textFile)
+                && methods.isCharacter(args)) {
+            
+            methods.countOccurrences(args, textFile);
+        }
+
+    }
+
+}
